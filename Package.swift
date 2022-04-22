1326132d81af991d9d74dbc0e0ae3098963f0190
Tomoaki Yagishita <tyagishi@gmail.com>
2022-04-22 18:02:37 +0900

    add missing dependency SDSViewExtension

--- Package.swift
+++ Package.swift
@@ -18,14 +18,15 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/tyagishi/SDSCGExtension", .upToNextMajor(from: .init(1, 1, 0)))
+        .package(url: "https://github.com/tyagishi/SDSCGExtension", .upToNextMajor(from: .init(1, 1, 0))),
+        .package(url: "https://github.com/tyagishi/SDSViewExtension", .upToNextMajor(from: .init(3, 0, 0)))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SDSCustomView",
-            dependencies: ["SDSCGExtension"]),
+            dependencies: ["SDSCGExtension", "SDSViewExtension"]),
 //        .testTarget(
 //            name: "SDSCustomViewTests",
 //            dependencies: ["SDSCustomView"]),
