b9231eaf663d5ae6420cdb65fe0e3d79c79885fd
Tomoaki Yagishita <tyagishi@gmail.com>
2022-07-22 14:06:35 +0900

    "new" HierarchicalReorderableForEach move TreeNode to SDSDataStructure for further re-use

--- Package.swift
+++ Package.swift
@@ -20,14 +20,15 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/tyagishi/SDSCGExtension", .upToNextMajor(from: .init(1, 1, 0))),
         .package(url: "https://github.com/tyagishi/SDSViewExtension", .upToNextMajor(from: .init(3, 0, 0))),
-        .package(url: "https://github.com/tyagishi/SwiftUIDebugUtil", .upToNextMajor(from: .init(2, 0, 0)))
+        .package(url: "https://github.com/tyagishi/SwiftUIDebugUtil", .upToNextMajor(from: .init(2, 0, 0))),
+        .package(url: "https://github.com/tyagishi/SDSDataStructure", .branch("main"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SDSCustomView",
-            dependencies: ["SDSCGExtension", "SDSViewExtension", "SwiftUIDebugUtil"]),
+            dependencies: ["SDSCGExtension", "SDSViewExtension", "SwiftUIDebugUtil", "SDSDataStructure"]),
 //        .testTarget(
 //            name: "SDSCustomViewTests",
 //            dependencies: ["SDSCustomView"]),
---
+++ Sources/SDSCustomView/HierarchicalReorderableForEach/HierarchicalReorderableForEach.swift
@@ -0,0 +1,164 @@
+//
+//  HierarchicalReorderableForEach.swift
+//
+//  Created by : Tomoaki Yagishita on 2022/07/22
+//  © 2022  SmallDeskSoftware
+//
+
+import SwiftUI
+import UniformTypeIdentifiers
+import SDSDataStructure
+
+let dragType = [UTType.text]
+
+public struct HierarchicalReorderableForEach<T: Equatable>: View {
+    @ObservedObject var current: TreeNode<T>
+    let childKey: ReferenceWritableKeyPath<TreeNode<T>, [TreeNode<T>]>
+    @Binding var draggingItem: TreeNode<T>?
+    var moveAction: (IndexPath, IndexPath) -> Void
+
+    public init( current: TreeNode<T>,
+          childKey: ReferenceWritableKeyPath<TreeNode<T>,[TreeNode<T>]>,
+          draggingItem: Binding<TreeNode<T>?>,
+          moveAction: @escaping (IndexPath, IndexPath) -> Void) {
+        self.current = current
+        self.childKey = childKey
+        self._draggingItem = draggingItem
+        self.moveAction = moveAction
+    }
+    public var body: some View {
+        ForEach(current[keyPath: childKey]) { child in
+            HierarchicalReorderableRow(child, \.children, $draggingItem, moveAction: { (from, to) in
+                current.rootNode().move(from: from, to: to)
+            }, content: { treeNode in
+                Text(treeNode.value as? String ?? "NoText")
+            })
+        }
+        // FIXME: onInsert does not work well for ForEach which is embedded in another ForEach....
+//        .onInsert(of: dragType) { index, providers in
+//            guard let draggingItem = draggingItem else { return }
+//            let currentIndex = current.indexPath()
+//            let fromIndex = draggingItem.indexPath()
+//            print("onInsert node: \(fromIndex) to: \(index) under: \(currentIndex)")
+//            moveAction(fromIndex, fromIndex)
+//        }
+    }
+}
+
+struct HierarchicalReorderableRow<T: Equatable, Content: View>: View {
+    @ObservedObject var node: TreeNode<T>
+    let childKey: ReferenceWritableKeyPath<TreeNode<T>, [TreeNode<T>]>
+    @Binding var draggingItem: TreeNode<T>?
+    let content: (TreeNode<T>) -> Content
+    var moveAction: (IndexPath, IndexPath) -> Void
+
+    @State private var expand = false
+    
+    public init(_ node: TreeNode<T>,
+                _ childKey: ReferenceWritableKeyPath<TreeNode<T>, [TreeNode<T>]>,
+                _ draggingItem: Binding<TreeNode<T>?>,
+                moveAction: @escaping (IndexPath, IndexPath) -> Void,
+                @ViewBuilder content: @escaping (TreeNode<T>) -> Content ) {
+        self.node = node
+        self.childKey = childKey
+        self._draggingItem = draggingItem
+        self.moveAction = moveAction
+        self.content = content
+        
+    }
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            HStack {
+                Image(systemName: "chevron.right").rotationEffect(expand ? .degrees(90) : .degrees(0))
+                    .onTapGesture {
+                        expand.toggle()
+                    }
+                content(node).frame(maxWidth: .infinity, alignment: .leading)
+//                    .overlay(draggingItem?.id == node.id ? Color.white.opacity(0.8) : Color.clear)
+            }
+//            .contentShape(Rectangle())
+            .onDrag {
+                self.draggingItem = node
+                return NSItemProvider(object: ((node.value as? String) ?? "NoText") as NSString)
+            }
+            .onDrop(of: dragType, delegate: DragDelegate<T>(root: node.rootNode(),
+                                                            item: node,
+                                                            draggingItem: $draggingItem,
+                                                            moveAction: moveAction))
+            .padding(.bottom, 8)
+            if expand,
+               !node.children.isEmpty {
+                HierarchicalReorderableForEach(current: node, childKey: \.children,
+                                               draggingItem: $draggingItem, moveAction: {(_,_) in })
+            }
+        }
+        .padding(.leading, CGFloat((node.indexPath().count - 1) * 8))
+    }
+}
+
+struct DragDelegate<T>: DropDelegate {
+    @ObservedObject var root: TreeNode<T>
+    let item: TreeNode<T>
+    @Binding var draggingItem: TreeNode<T>?
+    var moveAction: (IndexPath, IndexPath) -> Void
+
+    init(root: TreeNode<T>,
+         item: TreeNode<T>,
+         draggingItem: Binding<TreeNode<T>?>,
+         moveAction: @escaping (IndexPath, IndexPath) -> Void ) {
+        self.root = root
+        self.item = item
+        self._draggingItem = draggingItem
+        self.moveAction = moveAction
+    }
+    
+    func dropEntered(info: DropInfo) {
+        guard let draggingItem = draggingItem else { return }
+        guard draggingItem.id != item.id else { return }
+        print("dropEntered")
+
+        if draggingItem.isAncestor(of: item) { return }
+        
+        let fromNodeIndex = draggingItem.indexPath()
+        var toNodeIndex = item.indexPath()
+        
+        print("DropEntered from: \(fromNodeIndex) to: \(toNodeIndex)")
+        
+        if item.children.isEmpty {
+            // drop empty node, add as child
+            toNodeIndex.append(IndexPath(index: 0))
+        }
+        if fromNodeIndex != toNodeIndex {
+            moveAction(fromNodeIndex, toNodeIndex)
+            //root.move(from: fromNodeIndex, to: toNodeIndex)
+        }
+    }
+    
+//    func validateDrop(info: DropInfo) -> Bool {
+//        guard let draggingItem = draggingItem else { return false }
+//        //guard draggingItem.id != item.id else { return false }
+//        return true
+//    }
+    func performDrop(info: DropInfo) -> Bool {
+        //print("performDrop")
+        guard let draggingItem = draggingItem else { return false }
+//        if item.id == draggingItem.id {
+        self.draggingItem = nil
+//        }
+        return true
+    }
+    
+    func dropUpdated(info: DropInfo) -> DropProposal? {
+//        print("dropUpdated at \(item.value)")
+        guard let draggingItem = draggingItem else { return nil }
+        if item.id == draggingItem.id { return nil }
+        if draggingItem.isAncestor(of: item) { return nil }
+        return DropProposal(operation: .move)
+    }
+    
+//    func dropExited(info: DropInfo) {
+//        print("dropExited")
+//        guard let draggingItem = draggingItem else { return }
+//    }
+}
