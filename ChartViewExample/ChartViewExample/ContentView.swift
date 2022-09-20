ac2bbc6fe239d3c8ed6d50755e1481b46e883add
Tomoaki Yagishita <tyagishi@gmail.com>
2022-09-20 14:10:13 +0900

    add legend

--- ChartViewExample/ChartViewExample/ContentView.swift
+++ ChartViewExample/ChartViewExample/ContentView.swift
@@ -29,16 +29,26 @@ struct ContentView: View {
             .padding()
     }
     
-    func prepChartData(_ canvasSize: CGSize) -> (GraphData, AxisInfo) {
+    func prepChartData(_ size: CGSize) -> (GraphData, AxisInfo) {
         let xRange = (0.0)...(100.0)
         let graphData = DataPoint.randomSample(10, yRange: 10...100, xScale: 10, yScale: 1)
 
-        let polylineGraph = PolylineGraphDatum(graphData, size: canvasSize, )
-        
-        let polylineGraphDatum = PolylineGraphDatum(graphData, color: .red, vertexSymbol: {EmptyView().anyView()},
-                                                    xValueRange: (-20)...(120), yValueRange: -10...120, size: canvasSize)
-        let xAxisInfo = AxisInfo(axisValue: 0, color: .red, labelValues: [0,20,40,60,80,100], gridValues: [20, 40, 60, 80, 100], gridColor: .yellow.opacity(0.5))
-        let yAxisInfo = AxisInfo(axisValue: 0, color: .red, labelValues: [0,20,40,60,80,100], gridValues: [25, 50, 75, 100], gridColor: .green.opacity(0.3))
+        let canvas = PolylineGraphDatum.canvasFor(size, dataPoints: graphData,
+                                                  edgeInsetRatio: EdgeInsets(top: 0.1, leading: 0.1, bottom: 0.1, trailing: 0.1))
+
+        let polylineGraphDatum = PolylineGraphDatum(graphData, canvas: canvas)
+
+        let xAxisInfo = AxisInfo(axisValue: 20, color: .red, gridValues: [20, 40, 60, 80, 100], gridColor: .yellow.opacity(0.5), labelValues: [0,20,40,60,80,100],
+                                 labelContent: { labelValue in
+            Text(String("\(labelValue)"))
+                .offset(x: 0, y: -100)
+                .anyView()
+        })
+        let yAxisInfo = AxisInfo(axisValue: 0, color: .green, gridValues: [25, 50, 75, 100], gridColor: .green.opacity(0.3), labelValues: [0,20,40,60,80,100],
+                                 labelContent: { labelValue in
+            Text(String("\(labelValue)"))
+                .anyView()
+        })
 
         polylineGraphDatum.yAxisInfo = yAxisInfo
         return (GraphData([polylineGraphDatum], xRange: xRange), xAxisInfo)
