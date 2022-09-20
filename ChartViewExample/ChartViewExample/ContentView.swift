2b910f703922687e6fae18e2c37da24420493c6f
Tomoaki Yagishita <tyagishi@gmail.com>
2022-09-20 19:56:15 +0900

    update

--- ChartViewExample/ChartViewExample/ContentView.swift
+++ ChartViewExample/ChartViewExample/ContentView.swift
@@ -14,16 +14,10 @@ struct ContentView: View {
     var body: some View {
         GeometryReader{ geom in
             let (data, xAxisInfo) = prepChartData(geom.size)
-            Charts(data,
+            Charts(data, size: geom.size,
                    title: { Text("Title").font(.largeTitle).frame(maxHeight: .infinity, alignment: .top) },
-                   //legend: {EmptyView()},
+                   legend: {Text("Legend").frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topTrailing).padding(.top, 50)},
                    xAxis: xAxisInfo)
-//                   xAxis: .plotValue(AxisInfo(axisValue: 0, axisEnds: (0,110),
-//                                              color: .green, labelValues: [0,25,50,75,100], formatter: {value in String("\(value)") })),
-//                   yAxis: .plotValue(AxisInfo(axisValue: 0, axisEnds: (0, 120),
-//                                              color: .red, labelValues: [0,20,40,60,80,100], formatter: { value in String("\(value)") })),
-//                   xGrid: .plotValues(GridInfo(axisValues: [20, 40, 60, 80, 100], axisEnds: (0,100), color: .yellow.opacity(0.5))),
-//                   yGrid: .plotValues(GridInfo(axisValues: [25, 50, 75, 100], axisEnds: (0,100), color: .green.opacity(0.3))))
 
         }
             .padding()
@@ -41,13 +35,13 @@ struct ContentView: View {
 
         let polylineGraphDatum = PolylineGraphDatum(graphData, canvas: canvas)
 
-        let xAxisInfo = AxisInfo(axisValue: 0, color: .red, gridValues: [20, 40, 60, 80, 100], gridColor: .red.opacity(0.5), labelValues: [25, 50, 75, 100],
+        let xAxisInfo = AxisInfo(axisValue: 0, color: .red, gridValues: [20, 40, 60, 80, 100], gridColor: .red.opacity(0.5), labelValues: [20, 40, 60, 80, 100],
                                  labelContent: { labelValue in
             Text(String("\(labelValue)"))
                 //.offset(x: -10, y: 0)
                 .anyView()
         })
-        let yAxisInfo = AxisInfo(axisValue: 0, color: .green, gridValues: [25, 50, 75, 100], gridColor: .green.opacity(0.3), labelValues: [0, 20, 40, 60, 80, 100],
+        let yAxisInfo = AxisInfo(axisValue: 0, color: .green, gridValues: [25, 50, 75, 100], gridColor: .green.opacity(0.3), labelValues: [25, 50, 75, 100],
                                  labelContent: { labelValue in
             Text(String("\(labelValue)"))
                 //.offset(x: 0, y: -10)
