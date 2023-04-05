cd1f342ee00a99bae1c1f2453fe0d4750a3c664e
Tomoaki Yagishita <tyagishi@gmail.com>
2023-04-06 08:19:06 +0900

    update dependency version

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/tyagishi/SDSCGExtension", from: "1.1.0"),
-        .package(url: "https://github.com/tyagishi/SDSViewExtension", from: "3.0.0"),
+        .package(url: "https://github.com/tyagishi/SDSViewExtension", from: "4.0.0"),
         .package(url: "https://github.com/tyagishi/SwiftUIDebugUtil", from: "2.0.0"),
         .package(url: "https://github.com/tyagishi/SDSDataStructure", from: "1.0.0"),
     ],
