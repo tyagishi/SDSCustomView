88d8a925e037eab4de95b06048701d139f780d94
Tomoaki Yagishita <tyagishi@gmail.com>
2024-04-13 09:57:52 +0900

    F!! add indirect option & view modifier for it

--- Sources/SDSCustomView/EditableText/EditableText.swift
+++ Sources/SDSCustomView/EditableText/EditableText.swift
@@ -7,7 +7,11 @@
 
 import SwiftUI
 
+
 public struct EditableText: View {
+    public static var undoIcon = Image(systemName: "arrow.uturn.backward")
+    
+    @Environment(\.editableTextIndirect) var indirectEdit
     @Binding var value: String
     let alignment: Alignment
     @State private var underEditing = false {
@@ -17,7 +21,8 @@ public struct EditableText: View {
     let editClick: Int
     let placeholder: String
     let editIcon: Image
-    
+    @State private var indirectText: String
+
     public init(value: Binding<String>, 
                 placeholder: String = "",
                 editIcon: Image = Image(systemName: "pencil"),
@@ -27,20 +32,35 @@ public struct EditableText: View {
         self.editIcon = editIcon
         self.alignment = alignment
         self.editClick = editClick
+        
+        indirectText = value.wrappedValue
     }
     
     public var body: some View {
+        let binding = Binding<String>(get: {
+            if indirectEdit.flag { return indirectText }
+            return value
+        }, set: { newText in
+            if indirectEdit.flag { indirectText = newText; return }
+            value = newText
+        })
+        
         HStack {
             if underEditing {
                 TextField(placeholder,
-                          text: $value)
+                          text: binding)
                     .focused($fieldFocus)
-                    .onSubmit { underEditing.toggle() }
+                    .onSubmit { toggleUnderEditing() }
+                if indirectEdit.flag {
+                    Button(action: {
+                        indirectText = value
+                        underEditing.toggle()}, label: { indirectEdit.image })
+                }
             } else {
                 Text(value)
                     .frame(maxWidth: .infinity, alignment: alignment)
                     .contentShape(Rectangle())
-                    .onTapGesture(count: editClick, perform: { underEditing.toggle() })
+                    .onTapGesture(count: editClick, perform: { toggleUnderEditing() })
             }
             Button(action: { underEditing.toggle()}, label: { editIcon })
         }
@@ -48,7 +68,34 @@ public struct EditableText: View {
             if !fieldFocus { underEditing = false }
         }
     }
+    
+    func toggleUnderEditing() {
+        if indirectEdit.flag == true,
+           underEditing == true {
+            value = indirectText
+        }
+        underEditing.toggle()
+    }
 }
 #Preview {
     EditableText(value: .constant("Hello world"))
+}
+
+// MARK: indirectEdit ViewModifier
+struct EditableTextIndirectKey: EnvironmentKey {
+    typealias Value = (Bool, Image)
+    static var defaultValue: (Bool, Image) = (true, EditableText.undoIcon)
+}
+
+extension EnvironmentValues {
+    var editableTextIndirect: (flag: Bool, image: Image) {
+        get { return self[EditableTextIndirectKey.self] }
+        set { self[EditableTextIndirectKey.self] = newValue }
+    }
+}
+
+extension EditableText {
+    public func indirectEdit(_ flag: Bool = true, cancelImage: Image = EditableText.undoIcon) -> some View {
+        self.environment(\.editableTextIndirect, (flag, cancelImage))
+    }
 }
