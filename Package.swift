30517318f2cb4aea7c1183b74235f22aa11143ea
Tomoaki Yagishita <tyagishi@gmail.com>
2024-08-11 10:00:03 +0900

    fix swiftlint version

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         .package(url: "https://github.com/tyagishi/SDSCGExtension", from: "1.1.0"),
         .package(url: "https://github.com/tyagishi/SDSViewExtension", from: "4.0.0"),
         .package(url: "https://github.com/tyagishi/SDSDataStructure", from: "3.0.0"),
-        .package(url: "https://github.com/SimplyDanny/SwiftLintPlugins", from: "0.54.0"),
+        .package(url: "https://github.com/SimplyDanny/SwiftLintPlugins", exact: "0.56.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
