ca781ce9872afee0d593f14188d20c6f33200057
Tomoaki Yagishita <tyagishi@gmail.com>
2024-09-16 12:03:51 +0900

    F - fix typo

--- Sources/SDSCustomView/LazyView/LazyView.swift
+++ Sources/SDSCustomView/LazyView/LazyView.swift
@@ -7,12 +7,10 @@
 
 import SwiftUI
 
-public static accessCheck: Int = 4
-
 public struct LazyView<Content: View>: View {
     let buildContent: () -> Content
 
-    public init(_ @ViewBuilder buildContent: @autoclosure @escaping() -> Content ) {
+    public init(_ buildContent: @autoclosure @escaping() -> Content ) {
         self.buildContent = buildContent
     }
 
